f5cbfaba91baeaa36b887f58e9e22cfd1c37d3bb
hg <jakobjmnet@gmail.com>
2012-02-07 17:19:13 +0100

    remastered

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,7 +25,7 @@ function refreshFeed()
             for(var i = 0; i < feed.items.length && i < 10; i++)
             {
                 var item = feed.items[i];
-                $("#page2 ul").append('<li><a href="'+item.link+'">'+item.title+'</a></li>');
+                $("#page2 ul").append('<li><a href="'+item.link+'"><h3 class="ui-li-heading">'+item.title+'</h3><p class="ui-li-desc">'+item.description+'</p></a></li>');
             }
 
             $('#page2 ul').listview('refresh');
